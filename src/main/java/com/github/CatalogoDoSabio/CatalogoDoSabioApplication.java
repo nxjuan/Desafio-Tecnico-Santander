ad4289e0c9b659fbdac432bc72d7e70b9622334e
Juan Nogueira <juannogueiraxavier@gmail.com>
2025-01-11 23:13:14 -0300

    up

---
+++ src/main/java/com/github/CatalogoDoSabio/CatalogoDoSabioApplication.java
@@ -0,0 +1,13 @@
+package com.github.CatalogoDoSabio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDoSabioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoDoSabioApplication.class, args);
+	}
+
+}
